632df47cab00c04b66e81f9e34c0b57892824755
matheus-camilo-dev <matheuscamilovelanzuelameira@gmail.com>
2021-09-16 22:01:25 -0300

    Alteração para arquivos estáticos

--- igs_software_manager/settings.py
+++ igs_software_manager/settings.py
@@ -111,7 +111,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/',
 ]
 
 # Default primary key field type
